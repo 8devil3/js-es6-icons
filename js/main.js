9e6a3da18df3e8cdfa0a98936abd0fbbb4082b00
8devil3 <8dev.il3@gmail.com>
2022-03-09 18:26:03 +0100

    aggiunto "all" al filtro

--- js/main.js
+++ js/main.js
@@ -2,43 +2,59 @@ const main = document.querySelector("main");
 const header = document.querySelector("header");
 const selectIcon = document.querySelector("#select-icon");
 
-uniqueType();
 
+
+uniqueType();
+printAll();
+
+//stampa di tutte le icone
+function printAll() {
+    iconsArr.forEach((icon) => {
+        printIcons(icon.name, icon.prefix, icon.color);
+    });
+}
+
+
+//click sul menu a tendina
+selectIcon.addEventListener("change", () => {
+    //reset risultati
+    let iconsSelected = [];
+    main.innerHTML = "";
+
+    if (selectIcon.value == "all") {
+        printAll();
+    } else {
+        //filtro i risultati in base alla scelta dell'utente
+        iconsSelected = iconsArr.filter((element) => {
+            return element.type == selectIcon.value;
+        });
+
+        //genero le icone nei loro div
+        iconsSelected.forEach((icon) => {
+            printIcons(icon.name, icon.prefix, icon.color);
+        });
+    }
+});
+
+
+//generazione array con i type icon univoci
 function uniqueType() {
-    //genero l'array con i type icon univoci
     iconTypesUnique = iconsArr
         .map((icon) => {
             return icon.type;
         })
         .filter((element, index, array) => {
             return array.indexOf(element) == index;
-        }).sort();
+        });
+
+    iconTypesUnique.push("all");
+    iconTypesUnique.sort();
 
     //genero il menu a tendina HTML di type icon
     iconTypesUnique.forEach((element) => {
         selectTypeGen(element);
     });
 }
-
-//click sul menu a tendina
-selectIcon.addEventListener("change", () => {
-    //reset risultati
-    let iconsSelected = [];
-    main.innerHTML = '';
-
-    //filtro i risultati in base alla scelta dell'utente
-    iconsSelected = iconsArr.filter((element) => {
-        return element.type == selectIcon.value;
-    });
-    
-    //genero le icone nei loro div
-    iconsSelected.forEach((icon) => {
-        printIcons(icon.name, icon.prefix, icon.color);
-    });
-
-    console.log(iconsSelected);
-});
-
 
 
 //stampa icone nei div
@@ -48,7 +64,6 @@ function printIcons(iconName, iconPrefix, iconColor) {
     iconBox.innerHTML = `<i class="fa-solid ${iconPrefix}${iconName}" style="color: ${iconColor}"></i><p class="icon-name">${iconName}</p>`;
     main.appendChild(iconBox);
 }
-
 
 
 //stampa menu a tendina delle icon type
