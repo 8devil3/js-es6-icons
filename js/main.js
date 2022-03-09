07a04a9be90dde08fd9905f0523788da92ef673b
8devil3 <8dev.il3@gmail.com>
2022-03-09 18:19:01 +0100

    aggiunta funzione filtro, manca l'elemento "all"

--- js/main.js
+++ js/main.js
@@ -1,15 +1,61 @@
-const mainDiv = document.querySelector('main');
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const selectIcon = document.querySelector("#select-icon");
 
+uniqueType();
 
+function uniqueType() {
+    //genero l'array con i type icon univoci
+    iconTypesUnique = iconsArr
+        .map((icon) => {
+            return icon.type;
+        })
+        .filter((element, index, array) => {
+            return array.indexOf(element) == index;
+        }).sort();
 
+    //genero il menu a tendina HTML di type icon
+    iconTypesUnique.forEach((element) => {
+        selectTypeGen(element);
+    });
+}
 
-iconsArr.forEach(icon => {
-    printIcons(icon.name, icon.prefix, icon.color);
+//click sul menu a tendina
+selectIcon.addEventListener("change", () => {
+    //reset risultati
+    let iconsSelected = [];
+    main.innerHTML = '';
+
+    //filtro i risultati in base alla scelta dell'utente
+    iconsSelected = iconsArr.filter((element) => {
+        return element.type == selectIcon.value;
+    });
+    
+    //genero le icone nei loro div
+    iconsSelected.forEach((icon) => {
+        printIcons(icon.name, icon.prefix, icon.color);
+    });
+
+    console.log(iconsSelected);
 });
 
-function printIcons(iconName, iconPrefix, iconColor){
-    let iconBox = document.createElement('div');
-    iconBox.classList.add('icon-box');
+
+
+//stampa icone nei div
+function printIcons(iconName, iconPrefix, iconColor) {
+    let iconBox = document.createElement("div");
+    iconBox.classList.add("icon-box");
     iconBox.innerHTML = `<i class="fa-solid ${iconPrefix}${iconName}" style="color: ${iconColor}"></i><p class="icon-name">${iconName}</p>`;
-    mainDiv.appendChild(iconBox);
+    main.appendChild(iconBox);
+}
+
+
+
+//stampa menu a tendina delle icon type
+function selectTypeGen(iconType) {
+    let optIcon = document.createElement("option");
+
+    optIcon.value = iconType;
+    optIcon.innerText = iconType;
+    selectIcon.appendChild(optIcon);
 }
