dc07bb1a3ccaa5cbb12d4c5b653c6052ee679dde
8devil3 <8dev.il3@gmail.com>
2022-03-09 16:23:52 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const mainDiv = document.querySelector('main');
 
 
 iconsArr.forEach(icon => {
-    printIcons(icon.name, icon.prefix);
+    printIcons(icon.name, icon.prefix, icon.color);
 });
 
-function printIcons(iconName, iconPrefix){
+function printIcons(iconName, iconPrefix, iconColor){
     let iconBox = document.createElement('div');
     iconBox.classList.add('icon-box');
-    iconBox.innerHTML = `<i class="fa-solid ${iconPrefix}${iconName}"></i><p class="icon-name">${iconName}</p>`;
+    iconBox.innerHTML = `<i class="fa-solid ${iconPrefix}${iconName}" style="color: ${iconColor}"></i><p class="icon-name">${iconName}</p>`;
     mainDiv.appendChild(iconBox);
 }
